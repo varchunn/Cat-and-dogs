4aa5b7921400c22e2b3ce843a4194cdacf3a18cc
varchunn <varvaram186@gmail.com>
2025-04-06 21:34:00 +0300

    Initial Commit

---
+++ Cat and dogs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cat and dogs
+//
+//  Created by Варя Мінєнкова on 06.04.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
